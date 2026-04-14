01662bd6491fa275bb42fd207e3223057440b751
Lucas Miranda <104878497+LucasSMiranda@users.noreply.github.com>
2026-04-14 00:41:15 +0000

    Ajustes e adicionado número do computador

--- jogo_maior.c
+++ jogo_maior.c
@@ -3,28 +3,32 @@
 
 int main() {
 
-    int numeroJogador, nuemroComputador, resultado;
+    int numeroJogador, numeroComputador, resultado;
     char tipoComparacao;
 
     //Gerador de números aleatórios
 
     srand (time(0));
-    nuemroComputador - rand() % 100 + 1;//Número entre 1 e 100
+    numeroComputador = rand() % 100 + 1;//Número entre 1 e 100
 
     //Início do jogo
 
     printf("Bem vindo ao jogo de maior, menor ou igual.\n");
-    printf("Você deve escolher um número e um tipo de comparação.\n);
+    printf("Você deve escolher um número e um tipo de comparação.\n");
     printf("M. Maior\n");
     printf("N. Menor\n");
-    printf("I.Igual\n);
+    printf("I.Igual\n");
 
-    printf("Escolha a comparação:\n");
-    scanf("c", &tipoComparacao);
+    printf("Escolha a comparação:");
+    scanf(" %c", &tipoComparacao);
 
-    printf("Escolha o número (entre 1 e 100):\n");
-    scanf("d", &numeroJogador);
+    printf("Escolha o número (entre 1 e 100):");
+    scanf(" %d", &numeroJogador);
 
+    //Exibir o número do computador
+
+    printf("O número do computador é : %d\n", numeroComputador);
+   
 
     return 0;
 }
